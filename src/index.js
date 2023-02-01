3ca02af2d42edb20721629316193c9cc7256d696
Gsutna19 <fe.lipe19menezes@gmail.com>
2023-02-01 09:56:36 -0500

    Add navigation menu and simple styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import {heading, par} from './page-load.js';
+import {heading, par, ul} from './page-load.js';
+import './style.css';
+import Burger from './burger.jpg';
 
 function component() {
     const element = document.getElementById('content');
 
+    element.appendChild(ul);
     element.appendChild(heading);
     element.appendChild(par);
+
+    const myBurger = new Image();
+    myBurger.src = Burger;
+
+    element.appendChild(myBurger);
 
     return element;
 }
--- src/page-load.js
+++ src/page-load.js
@@ -1,7 +1,21 @@
-export { heading, par}
+export { heading, par, ul }
 
 const heading = document.createElement('h1');
 const par = document.createElement('p');
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+const li2 = document.createElement('li');
+const li3 = document.createElement('li');
 
-heading.textContent = "Welcome to Reastaurant!";
-par.textContent = "The restaurant is great! 10/10 would eat here again.";
+ul.classList.add('header');
+
+li.textContent = 'Home';
+li2.textContent = 'Menu';
+li3.textContent = 'Contact Us';
+
+ul.appendChild(li);
+ul.appendChild(li2);
+ul.appendChild(li3);
+
+heading.innerHTML = "Welcome to Restaurant!";
+par.innerHTML = "The restaurant is great! 10/10 would eat here again.";
